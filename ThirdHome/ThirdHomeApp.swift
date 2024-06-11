06d3302fc8fae7d9600acd9a2613cbfca14111f1
Trinity Tandon <tyty2019jat@gmail.com>
2024-06-11 09:51:00 -0500

    Initial Commit

---
+++ ThirdHome/ThirdHomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThirdHomeApp.swift
+//  ThirdHome
+//
+//  Created by Scholar on 6/11/24.
+//
+
+import SwiftUI
+
+@main
+struct ThirdHomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
